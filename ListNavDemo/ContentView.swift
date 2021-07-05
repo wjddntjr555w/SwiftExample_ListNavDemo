c4f470c15cdb87f0b666e9b7f3887e69cea30b35
Jeong WooSeok <wjddntjr555w@naver.com>
2021-07-05 20:36:56 +0900

    Initial Commit

---
+++ ListNavDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ListNavDemo
+//
+//  Created by 정우석 on 2021/07/05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
